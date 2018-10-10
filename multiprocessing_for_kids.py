3604a5b86ba36be89621353259736ce0e42afaf0
Sebastian Nichtern <18186962+svsn2117@users.noreply.github.com>
2018-10-10 19:39:28 +0200

    Initial Commit You can run your function on all processors using multiprocessing. An iterator that you give will define how often your function will be called. Please see my Blog post on predicted.blog/multiprocessing-for-kids or the multiprocessing_for_kids_examples.py for further details. I did a lot of testing but sure there are always some bugs left. Please feel free to pull request if you have optimized things or fixed a bug. My time for this project is really limited - so, if you would like to completely take over the maintanance as an admin let me know!

---
+++ multiprocessing_for_kids.py
@@ -0,0 +1,209 @@
+# With this package multiprocessing is made easy. Just call the doMultiprocessingLoop()
+# function and give it the function you want to execute in multiple processes, as well
+# as an iterator and static arguments. You can add shared variables that can be seen and
+# changed within all processes.
+# It's also possible to return a value (of nearly any type) from the function that is
+# looped over (loopFunction_). Therefore set terminateIfValReturned = True. This terminates
+# all processes when a result is returned by your function (e.g. a solution you where searching
+# for was found).
+# If terminateIfValReturned = False and values are returned by the loopFunction_, the
+# doMultiprocessingLoop() will return a list with the return values in each step after your
+# loop has finished. (However this is not tested enough to be 100% secure. Please make some
+# test on your own.)
+
+# We now can get python to use 100% of the CPU's instead of just one.
+
+# You can copy this file direktly into your project or into the site-packages folder of your
+# python distribution to make it accessible in all of your Python projects.
+from multiprocessing import Pool,Manager,managers,cpu_count,TimeoutError # ,Lock
+from ctypes import c_wchar_p
+import warnings
+
+NUM_OF_PARALLEL_PROCESSES = cpu_count() # for max performance use cpu_count().
+
+m = Manager()
+# sharedVariables = m.list() # this causes dificuilties when adding queue Vars. And i found that a normal List also dose the job:
+sharedVariables = [] # change them with variable.value = ...
+
+def __varToValue(var):
+    # Multiprocessing works with Values instead of normal Variables
+    # This Function does the transformation for you
+    if isinstance(var, (int, list)):
+        return m.Value('i',var)
+    elif isinstance(var, float):
+        return m.Value('d',var)
+    elif isinstance(var, str):
+        return m.Value(c_wchar_p, var)
+    elif isinstance(var, dict):
+        return m.dict(var)
+    elif type(var).__name__ == 'Queue':
+        q = m.Queue(var.maxsize+1)
+        # To get the queue as a list use the managerQueueToList() Function in this Module
+        # !But try _not_ to use this! Use a Dict if you need it as a List anywhere! See the managerQueueToList() for more Info
+        if not var.empty():
+            for element in iter(var.get, None): # transfer all queue elements to the manager queue
+                q.put(element)
+        return q
+    # set() is not available. Use Dict instead. [https://stackoverflow.com/a/37714759/7699319]
+    else:
+        print("This Type: \""+str(type(var).__name__)+"\" cannot be shared. Use List, int, float, String, Dict or Queue")
+
+def addSharedVars(*args):
+    global sharedVariables
+    for arg in args:
+        sharedVariables.append(__varToValue(arg))
+
+def setSharedVars(pos, val):
+    global sharedVariables
+    if type(sharedVariables[pos]).__name__ == 'ValueProxy':
+        sharedVariables[pos].value = __varToValue(val)
+    elif type(sharedVariables[pos]).__name__ == 'DictProxy':
+        sharedVariables[pos] = __varToValue(val)
+    elif pos>=len(sharedVariables):
+        print("can't set this shared Variable because the position"+ str(pos) +" dose not exist (index out of bounds). Function: setSharedVars in multiprocessing_me")
+    else:
+        print("This is not a Value or Dict. The given type is not jet defined in the setSharedVars function")
+
+def removeAllSharedVars():
+    global sharedVariables
+    sharedVariables = []
+
+def resetSharedVars():
+    global sharedVariables
+    for i,var in enumerate(sharedVariables):
+        if type(var).__name__ == 'ValueProxy':
+            if type(var.value).__name__ == 'int':
+                sharedVariables[i].value = 0
+            if type(var.value).__name__ == 'list':
+                sharedVariables[i].value = []
+            if type(var.value).__name__ == 'float':
+                sharedVariables[i].value = 0.0
+            if type(var.value).__name__ == 'str':
+                sharedVariables[i].value = ""
+        elif type(var).__name__ == 'DictProxy':
+            sharedVariables[i].clear()
+        # set() is not available. Use Dict instead. [https://stackoverflow.com/a/37714759/7699319]
+        else:
+            print("This Type: \"" + str(type(var).__name__) + "\" is not supported by the resetSharedVars() function!")
+
+def getSharedVars():
+    return [var for var in sharedVariables]
+
+def getSharedVarsAsValues():
+    retVals = []
+    for val in sharedVariables:
+        if type(val) == managers.ValueProxy:
+            retVals.append(val.value)
+        elif type(val) == managers.DictProxy:
+            retVals.append(dict(val.items()))
+        else:
+            retVals.append(val)
+    return retVals
+
+def managerQueueToList(queue, warning=True):
+    # Returns the shared queue as a list.
+    # this is _not_ a pretty good (+ slow) solution! Try to use Dict instead and check for its size
+    # it can also happen that another process is writing something new into the Queue while
+    # its refilled or emptied
+    if warning:
+        warnings.warn("This is a relatively slow method to convert the given Queue to a list. Please try to work with dicts if possible to avoid this.")
+    queue_as_list = []
+    while not queue.empty():
+        queue_as_list.append(queue.get())
+    for val in queue_as_list:
+        if not queue.full():
+            queue.put(val)
+    return queue_as_list
+
+
+terminated = False
+def doMultiprocessingLoop(loopFunction_, loopIterator, terminateIfValReturned=False, *args):
+    # loopFunction_: The Function that will be executed in a loop and in different processes. !Musst be a global Function! Functions in Functions don't work!
+    # loopIterator: A List of elements passed to each function/process
+    # *args: different Arguments that need to be passed to the Function and don't need to be shared (same in every loop iteration and process)
+
+    #if len(loopIterator) > 600:
+    #    warnings.warn("Error, loop iterator can only handle a limited amount of entries. "
+    #                  "You probably missunderstood the way multiprocessing works."
+    #                  "Only situations where the loopFunction_ takes a lot of computational power"
+    #                  "and are executed in a limeted fassion are feasible for Multiprocessing.")
+    #    return "Error, loop iterator can't have more than 600 entries"
+
+    p = Pool(NUM_OF_PARALLEL_PROCESSES)  # my Macbook e.g. has 4 CPU's # Programming fact: Pool() must be defined in the function where it's used!!
+    def __callback(e):
+        if terminateIfValReturned:
+            global terminated
+            if e:
+                print("Terminating Multiprocessing...")
+                terminated = True
+                p.terminate()
+
+    res = [None] * len(loopIterator) # Return Values. Is not actually needed if you use shared Vars..
+    for i, iterVal in enumerate(loopIterator):
+        res[i] = p.apply_async(loopFunction_, (iterVal, *args, *sharedVariables), callback=__callback)
+    p.close()
+    p.join()
+
+    # FOR DEBUGGING: (This way you can check if specific processes have finished)
+    # finished = [False] * len(loopIterator)
+    # while True:
+    #     sleep(0.0001)
+    #     ready = [r.ready() for r in res]
+    #     for i, rdy in enumerate(ready):
+    #         if (rdy and not finished[i]):
+    #             if PRINT_PROCESSES:
+    #                 print("Process " + str(i) + " is done.")
+    #             finished[i] = True
+    #     if (sum(ready) == len(loopIterator)):
+    #         break
+
+    ''' Wait for Results ''' # if join fails to wait properly (happend, trust me)
+    while True: # Checks if every Process is done.
+        # sleep(0.0001) # maybe?
+        if (sum([r.ready() for r in res]) == len(loopIterator)) or terminated:
+            break
+
+    ''' Return '''
+    results = []
+    for re in res: # all runs
+        try:
+            try:
+                r = re.get(timeout=1)
+            except TimeoutError: # A res Variable has nothing in it
+                if terminated:
+                    continue
+                print("Multiprocessing_me TimeoutError. One of the return Values is not here jet for some reason. It will be skipped. This should never happen.")
+                continue
+            if r is not None: # not None
+                if isinstance(r, (int, float, str)):
+                    results.append(r)
+                elif type(r).__module__ == 'numpy':
+                    results.append(r)
+                elif len(r) < 2: # only one return Variable
+                    if type(r).__name__ == 'ValueProxy':
+                        results.append(r.value)
+                    elif type(r).__name__ == 'DictProxy':
+                        results.append(dict(r.items()))
+                    else:
+                        results.append(r)
+                else:
+                    for val in r:  # All return Variables
+                        if type(val).__name__ == 'ValueProxy':
+                            results.append(r.value)
+                        elif type(val).__name__ == 'DictProxy':
+                            results.append(dict(val.items()))
+                        else:
+                            results.append(val)
+        except IndexError:
+            if terminated:
+                continue
+            print("Multiprocessing_me IndexError. One of the return Values will not be returned!") # I dont know why and I dont know a solution for this
+        if terminated and results: # results not empty
+            break # if terminated, ther will only be one result (the one from the callback)
+
+    # print("MP return", results)
+    return results
+
+
+''' Examples on how to use this Module: '''
+# Please see multiprocessing_for_kids_examples.py or my blog post here: predicted.blog/multiprocessing-for-kids
---
+++ multiprocessing_for_kids_examples.py
@@ -0,0 +1,168 @@
+# In this file I will present you some example code on how to use the Multiprocessing for Kids
+# package. I hope this makes it really easy for you to use it in your own projects
+
+# Just uncomment the example function you want to see on the bottom. There is also a detailed
+# description to every example. (inside the if __name__ == "__main__": statement)
+
+import Multiprocessing_for_kids as mulki
+import random
+import time
+
+''' Example 1: count 2 million '''
+# split in 10 tasks, each counting to 200 thousand
+# 3 static arguments
+# no shared variables
+# no return variable
+
+
+def countTo(iter_val, goal, steps, _print):
+    t0 = time.time()
+    step_range = int(goal / steps)               # 2 mil / steps (e.g. 10 steps a 200k each -> _range = 200k)
+    _from = (iter_val-1) * step_range            # e.g. step 1: 1-1 * 200.000 = 0
+    _to   = iter_val     * step_range            # e.g. step 1: 1 * 200.000 = 200.000
+
+    ''' the actual counting: '''
+    for i in range(_from, _to + 1):            # +1 to get to 200k instead of 199.999
+        if _print:
+            print(i)
+
+    print("Finished:", iter_val, " from ", _from, " to ", _to, "in", round(time.time()-t0,1), "s")
+
+
+def example1(_print = False, without_mp = False):
+    # In this first example we count in 10 steps to 2 million. Each step is handled by a
+    # different process. So our iterator counts from 1 to 10. We could also count in
+    # 100 episodes to 2 million. Just change the iterator to range(1,101)
+    print("Start...")
+    t_start_mp = time.time()
+    iterator = range(1,11)          # count 10 times to 200k -> 2 million
+    goal = 2000000                  # count to 2 mil
+    if not _print:                  # to see an effect when there is no printing
+        goal = goal * 1000          # we count to 2 billion instead of 2 million
+
+    # With Multiprocessing:
+    mulki.doMultiprocessingLoop(countTo, iterator, False, goal, len(iterator), _print)
+    print("Finished counting with Multiprocessing in ", round(time.time()-t_start_mp, 1), "s")
+
+    # Without Multiprocessing:
+    if without_mp:
+        t_start_normal = time.time()
+        countTo(1, goal, 1, _print)
+        print("Finished counting without Multiprocessing in ", round(time.time()-t_start_normal, 1), "s")
+
+
+
+''' Example 2: edit and print shared Variables '''
+# no static arguments
+# 2 shared variables
+# no return variable
+
+def changeSharedVar(iter_val, var1, var2):
+    # Important: Change the var.value property, not the var directly
+    for i in range(random.randrange(100,10000)):
+        var1.value += 1
+    print("var1 = ", var1.value)
+    var2.value += "o"
+    print("Process Nr:", iter_val, " var2 =", var2.value)
+
+def example2():
+    # Now we want to use two shared variables. Therefore, we count randomly to get
+    # different timeframes in each separate process. This way the variables are changed
+    # and printed at different times from different processes.
+    var1 = 0
+    var2 = "Hello"
+    mulki.addSharedVars(var1, var2)
+    mulki.doMultiprocessingLoop(changeSharedVar, range(10))
+
+
+''' Example 3: return as soon as a result was found '''
+# let the processes all do the same searching task and the first one who finds a solution
+# returns it.
+# 2 static arguments
+# 1 shared variable
+# 1 return variable
+
+def seach_the_number(_, the_number, search_range, attempt):
+    # !!! First the iterator then the static then the shared vars !!!
+    # The task is to find a given Number between 0 and 10.000
+    guess = 0
+    while guess != the_number:
+        attempt.value += 1
+        guess = random.randrange(search_range)
+    return guess
+
+def example3():
+    t_start = time.time()   # measure time
+
+    search_range = 10000    # modify this for longer searching times
+
+    the_number = random.randrange(search_range) # the process that finds this Number wins
+    print("Number to guess: ", the_number)
+
+    mulki.addSharedVars(0) # attempts as shared var
+    result = mulki.doMultiprocessingLoop(seach_the_number, range(100), True, the_number, search_range)
+
+    print("Correct guess:", result[0], "after", mulki.getSharedVarsAsValues()[0], "attempts [time:", round(time.time() - t_start, 1), "s]")
+
+
+
+
+if __name__ == "__main__":
+    example1() # uncomment this to see example 1. Read notes below
+    # For better understanding what the example is doing run example1(False).
+    # As we can see it counts from 0 to 2 billon in 10 (printed) steps. Each step runs
+    # in a separate process so we use 100% of CPU. If you don't see your CPU
+    # fully occupied, just increase the goal value in example1()
+
+    # As you run example1(False) you can see Steps "Finished: 2 ...", "Finished: 1.."
+    # finishing at kind of the same time. The number of steps finishing at (nearly) the same
+    # time is the amount of CPU's that are used. Each process counts a different range and each
+    # process has been running on a different processor. After the first e.g. 4 ranges are
+    # done counting through, the e.g. 4 CPU's are back available and start with the next
+    # 4 ranges (steps) of counting. That's why 4 steps always finish in (nearly) the same time
+    # (if you have 4 CPU's).
+
+    # Sometimes we see processes not finishing in the Order they are started. That's the
+    # prove that there are actually multiple processes running in parallel on different
+    # CPU's. We can even see the counting of the processes and the current values if we run
+    # example1(True) # (uncomment this) don't forget to comment example1() above
+    # As we can see, the order is mixed up.
+
+    # To Compare the time to "normal" counting on one CPU without multiprocessing, run:
+    # example1(False, True)
+
+    # If we don't want to split the counting task or we want to do the counting (or another
+    # task) in order, we have to use shared variables as we can see in example2.
+
+
+
+    # example2() # uncomment this and comment example1() above
+    # As we can see, the output (print) of var1 is always in order (increasing over time) even
+    # when the processes finished after different time frames (because counting to values
+    # between 100 and 10k).
+    # With this we have proven that all processes are manipulating the same shared variables
+    # Sharing variables also works with strings (var2), floats, lists,
+    # dicts, and queues (not recommended, see Multiprocessing_for_kids.py for more info)
+    # You can add more types by manipulating the "__varToValue()" function in
+    # Multiprocessing_for_kids.py
+
+
+
+    # example3()
+    # Here we are showing that the termination process with a return value (the result)
+    # works fine. So, whenever you need to do a calculation that requires a lot of time and
+    # resources but you don't know exactly when it's finished, you can set
+    # terminateIfValReturned = True and return from your function whenever it's done.
+    # This will trigger a callback, terminate all processes and return the return value.
+
+    # trying to make the results reproducible (e.g. with random.seed(1)) is not completely
+    # possible because the CPU's take different times for the calculations depending on their
+    # other tasks.
+
+    # Author: Sebastian Nichtern
+
+    ''' Ideas for future examples: ''' # feel free to implement and pull request
+    # 1. Return without termination after each process has finished (one run of the given
+    #    function)
+    # 2. Realworld practical usecase of multi processing (e.g. prime factorization, knn or
+    #    some other expensive algorithm)
